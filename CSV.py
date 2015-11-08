8cd2c0d090483821770d028741ae710e3ccce694
Shashank Srivastava <Shashank2406@outlook.com>
2015-11-08 19:21:56 +0530

    Update CSV.py

--- CSV.py
+++ CSV.py
@@ -1,77 +1,65 @@
 from Tkinter import *
 import csv
+
 def show():
 	x=e1.get()
 	y=e2.get()
 	print("Top %s\nYear %s"%(x,y))
 	fun1()
-def fun():
-	root = Tk() #Blank window
-	thelable = Label(root,text = "MiniProject Aplha 1")  #creates label window
-	thelable.pack()
-	root.mainloop()
+def fun5():
+	entr=Tk()
+	Label(entr,text='Enter Top').grid(row=0)
+	Label(entr,text='Enter Year').grid(row=1)
+	Label(entr,text='Male/Female/Both').grid(row=2)
+	global e1
+	e1=Entry(entr)
+	global e2
+	e2=Entry(entr)
+	global e3
+	e3=Entry(entr)
+	e1.grid(row=0,column=1)
+	e2.grid(row=1,column=1)
+	e3.grid(row=2,column=1)
+	Button(entr,text='Exit',command=entr.destroy).grid(row=3,column=0,sticky=W,pady=4)
+	Button(entr,text='Show',command=fun1).grid(row=3,column=1,sticky=W,pady=4)
+	entr.mainloop()
 def fun1():
-	root = Tk()
-	topframe = Frame(root)
-	topframe.pack()	
-	bottomframe = Frame(root)
-	bottomframe.pack(side=BOTTOM)
-	b=Button(topframe,text="Search",fg="black")
-	b1=Button(topframe,text="Search 1",fg="black")
-	b2=Button(topframe,text="Search 2",fg="black")
-	b3=Button(bottomframe,text="Search 3",fg="black")
-	b.pack(side=LEFT)
-	b1.pack(side=LEFT)
-	b2.pack(side=LEFT)
-	b3.pack(side=BOTTOM)
-	root.mainloop()
-def fun6():
-	f=open("a.csv")
+	x=(e3.get()) + '_cy' + (e2.get()) + '_top.csv'
+	print x 
+	f=open(x)
 	csv_f=csv.reader(f)
+	root = Tk()
+	T = Text(root, width=40, height=60)
+	S = Scrollbar(root)
+	S.pack(side=RIGHT, fill=Y)
+	T.pack(side=LEFT, fill=Y)
+	S.config(command=T.yview)
+	T.config(yscrollcommand=S.set)
+	x=e1.get()
 	for row in csv_f:
-		#if(row[2]<=e1.get()):
-	 		print row[2]
-	f.close()	
-
-def fun2():	
-	root = Tk() #Blank window
-	lable_1 = Label(root,text = "Name")  
-	lable_2 = Label(root,text = "MiniProject Aplha 2")
-	entry_1= Entry(root)
-	entry_2= Entry(root)
-	lable_1.grid(row=0,column=0,sticky=E) #default column is zero;
-	lable_2.grid(row=1,column=0)
-	entry_1.grid(row=0,column=1)
-	entry_2.grid(row=1,column=1)
-	root.mainloop()
-
+	 	y=row[2]
+	 	y=y.replace("=","")
+	 	if(y == "Position"):
+	 		print "hello"
+		else:
+			if(int (y) <= int (x)):	
+				T.insert(END,row)
+				T.insert(END,'\n')		
+	root.mainloop()	
 def fun4():
 	root = Tk()
 	frame= Frame(root,width=240, height=60)
-	thelable = Label(root,text = "MiniProject Aplha Phase 2")  #creates label window
-	button_1 = Button(root,text="Top Names?", command=fun5)
+	thelable = Label(root,text = "MiniProject Aplha Phase 3")  #creates label window
+	button_1 = Button(root,text="Top Names", command=fun5)
 	#button_2 = Button(root,text="Values Present", command=show)
-	button_3 = Button(root,text="Exit", command=root.quit)
+	button_3 = Button(root,text="Exit", command=root.destroy)
 	thelable.pack()
 	button_1.pack()
 	#button_2.pack()
 	button_3.pack()
 	frame.pack()
 	root.mainloop()
-def fun5():
-	entr=Tk()
-	Label(entr,text='Enter Top').grid(row=0)
-	Label(entr,text='Enter Year').grid(row=1)
-	global e1
-	e1=Entry(entr)
-	global e2
-	e2=Entry(entr)
-	e1.grid(row=0,column=1)
-	e2.grid(row=1,column=1)
-	Button(entr,text='Submit',command=entr.destroy).grid(row=3,column=0,sticky=W,pady=4)
-	Button(entr,text='show',command=show).grid(row=3,column=1,sticky=W,pady=4)	
 #fun()	
 #fun1()
 #fun2()
-#fun5()
 fun4()
